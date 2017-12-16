885d1dcf366681d2ae085028078338d0f52c6550
Michele Fiorito <fiore.ansaya@gmail.com>
2017-12-16 02:44:41 +0100

    PRAreaReducer partially implemented

--- Floorplanner/Solver/Reducers/IAreaReducer.cs
+++ Floorplanner/Solver/Reducers/IAreaReducer.cs
@@ -6,8 +6,18 @@ namespace Floorplanner.Solver.Reducers
 {
     public interface IAreaReducer
     {
+        /// <summary>
+        /// Function used to calculate area cost
+        /// </summary>
         Func<Area, Costs, int> CostFunction { get; set; }
 
+        /// <summary>
+        /// Reduces a given area as much as possible calculating cost with CostFunction.
+        /// Area with minimal cost is chosen.
+        /// </summary>
+        /// <param name="area">Area to reduce; it must be a valid and sufficient area for the associated region.</param>
+        /// <param name="idealCenter">Ideal precalculated area center point.</param>
+        /// <param name="floorPlan">Given area's associated floorplan object.</param>
         void Reduce(Area area, Point idealCenter, Floorplan floorPlan);
     }
 }
--- Floorplanner/Solver/Reducers/PRAreaReducer.cs
+++ Floorplanner/Solver/Reducers/PRAreaReducer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,62 @@ namespace Floorplanner.Solver.Reducers
 
         public void Reduce(Area area, Point idealCenter, Floorplan floorPlan)
         {
-            // TODO: reduce area tile per tile
+            int tileHeight = floorPlan.Design.FPGA.TileHeight;
 
+            // Store starting area position and dimensions
+            Point oldTopLeft = new Point(area.TopLeft);
+            int oldWidth = area.Width, oldHeight = area.Height;
 
-            throw new NotImplementedException();
+            // Get center tile row number
+            int centerTRow = (int)idealCenter.Y / tileHeight;
+
+            // If the area isn't only on centerTRow shrink it there
+            // or as close as possible
+            if(!area.TileRows.All(r => r == centerTRow))
+            {
+                // Shrink and move area to center tile row
+                area.Height = tileHeight - 1;
+                area.MoveTo(new Point(oldTopLeft.X, tileHeight * centerTRow));
+
+                if (!area.IsSufficient)
+                {
+                    // TODO: expand on a second/third/etc. tile row until area is again sufficient
+                    //       then shrink left and right only until minimum area
+                }
+
+                // Store better found area
+                oldTopLeft = new Point(area.TopLeft);
+                oldWidth = area.Width;
+                oldHeight = area.Height;
+            }
+
+            Direction shrinkDir = area.Center.X > idealCenter.X
+                ? Direction.Right : Direction.Left;
+
+            ShrinkOn(shrinkDir, area);
+
+            ShrinkOn(shrinkDir.Opposite(), area);
+        }
+
+        /// <summary>
+        /// Shrink area from given direction as much as possible.
+        /// After the call the area will still be valid and sufficient.
+        /// </summary>
+        /// <param name="shrinkDir">Direction to shrink from.</param>
+        /// <param name="area">Area to shrink.</param>
+        private void ShrinkOn(Direction shrinkDir, Area area)
+        {
+            // Try reducing on chosen direction until possible or resources are insufficient
+            while (area.IsSufficient)
+                if (!area.TryShape(Models.Solver.Action.Shrink, shrinkDir))
+                    break;
+
+            shrinkDir = shrinkDir.Opposite();
+
+            // Bring area back to valid and sufficient dimension after shrinking
+            while (!area.IsSufficient || !area.IsValid)
+                if (!area.TryShape(Models.Solver.Action.Expand, shrinkDir))
+                    break;
         }
     }
 }
